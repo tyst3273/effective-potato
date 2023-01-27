69f2bcbf15d290ce7d524d6840c51e5a749e737e
tyst3273 <ty.sterling@colorado.edu>
2023-01-27 12:52:07 -0700

    afasdfas

--- custom_phonx_interface/hdf5_access.py
+++ custom_phonx_interface/hdf5_access.py
@@ -104,16 +104,17 @@ class access_data_in_hdf5:
                 self.Q_points[:,2] = db['L'][...]
                 self.E = db['DeltaE'][...]
                 self.num_E_in_file = self.E.size
-                _H_bins = db['H_bins'][...]
-                _K_bins = db['K_bins'][...]
-                _L_bins = db['L_bins'][...]
-                _E_bins = db['E_bins'][...]
+                self.H_bins = db['H_bins'][...]
+                self.K_bins = db['K_bins'][...]
+                self.L_bins = db['L_bins'][...]
+                self.E_bins = db['E_bins'][...]
         except Exception as ex:
             msg = f'couldnt read Q and E from hdf5 file \'{self.file_name}\'.\n' \
                   'check the file and the h5py installation and try again.\n\n' \
                   'see the exception below for more hints.\n'
             crash(msg,ex)
 
+        _H_bins = self.H_bins; _K_bins = self.K_bins; _L_bins = self.L_bins; _E_bins = self.E_bins
         _Q_max = self.Q_points.max(axis=0); _Q_min = self.Q_points.min(axis=0)
         msg = f'\nfound {self.num_Q_in_file} Q-points and {self.num_E_in_file} E-points in file\n\n'
         msg += 'range of reciprocal space included in file:\n'
@@ -191,32 +192,72 @@ class access_data_in_hdf5:
         """
         _t = c_timer('get_signal_and_error')
 
-        _n = 10; _eps = 1e-3
+        _cutoff = 0.05; _prec = 4; _eps = 0.01
+        
+        msg = f'\n-------------------------------------------------------------------------\n'
+        msg += f'attempting to get data at Q = ({Q[0]: .3f},{Q[1]: .3f},{Q[2]: .3f})\n'
+        print(msg)
 
+        # get distance from Q(user) to all Qpts in file
         _Qpts = self.Q_points
         _Q2Qp = self.Q_to_Qp
         _d = self.dist_to_Qp
-
-        _Q2Qp[...] = 0.0
-        _d[...] = 0.0
-
-        # get distance from Q(user) to all Qpts in file
         _Q2Qp[:,0] = _Qpts[:,0]-Q[0]
         _Q2Qp[:,1] = _Qpts[:,1]-Q[1]
         _Q2Qp[:,2] = _Qpts[:,2]-Q[2]
-        _d[...] = np.sqrt(np.sum(_Q2Qp**2,axis=1))
+        _d[...] = np.round(np.sqrt(np.sum(_Q2Qp**2,axis=1)),_prec)
 
-        # find the closest ones
-        _inds = np.argsort(_d)[:_n]
+        # find the closest Q-point
+        Q_ind = np.argsort(_d)[0]
 
-        print(_inds)
-        print(_Qpts[_inds])
-        print(Q)
-        print(_d[_inds])
+        # return empty arrays if no Q-point within distance _cutoff of Q
+        if _d[Q_ind] >= _cutoff:
+            msg = 'no slice. continuing ...\n'
+            print(msg)
+            _t.stop()
 
-        print('')
+            # return signal and err arrays filled with nans
+            return np.full(self.num_E_in_file,np.nan), np.full(self.num_E_in_file,np.nan)
+
+        # print a warning if this Q-point isnt exaclty the same as requested by user
+        _Q = _Qpts[Q_ind]
+        for ii in range(3):
+            if np.abs(_Q[ii]-Q[ii]) >= _eps:
+                msg = '\n*** WARNING ***\n'
+                msg += 'nearest Q-point is not exactly what is requested!\n'
+                msg += f'nearest Q = ({_Q[0]: .3f},{_Q[1]: .3f},{_Q[2]: .3f})\n'
+                msg += 'if this is not what is expected, pick a Q-point commensurate with the\n' \
+                       'data in the file and run again.\n'
+                print(msg)
+
+        # now get and return the data
+        sig, err = self._get_cut_from_hdf5(Q_ind)
+
         _t.stop()
-        print('')
+
+        return sig, err
+
+    # ----------------------------------------------------------------------------------------------
+
+    def _get_cut_from_hdf5(self):
+        """
+        attempt to get the data from hdf5 file. if fails, return nans
+        """
+        try:
+            with h5py.File(self.file_name,'r') as db:
+                print(db.keys())
+                sig = db['signal'][Q_ind,...]
+                err = db['error'][Q_ind,...]
+        except Exception as ex:
+            msg = '\n*** WARNING ***\n'
+            msg += f'couldnt signal/error from hdf5 file \'{self.file_name}\'.\n' \
+                  'see the exception below for what went wrong.\n\n' \
+                  'continuing, but this is bad! I hope you know what you are doing ...\n' 
+            msg += '\nException:\n'+str(exception)+'\n'
+            print(msg)
+            sig = np.full(self.num_E_in_file,np.nan); err = np.full(self.num_E_in_file,np.nan)
+
+        return sig, err
 
     # ----------------------------------------------------------------------------------------------
 
@@ -230,7 +271,12 @@ if __name__ == '__main__':
     hdf5_file_name = 'LSNO25_300K_parallel.hdf5'
     access_tools = access_data_in_hdf5(hdf5_file_name)
 
-    access_tools.get_signal_and_error([0.1,0.1,0.1])
+    Q = [[ 6.0, 0.2,  0.0],
+         [-5.0, 0.2,  0.0],
+         [ 6.0, 0.21, 6.25]]
+
+    for QQ in Q:
+        access_tools.get_signal_and_error(QQ)
 
 
 
