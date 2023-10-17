0fd93da84ac56ef151974f386dabd0b80b85f4af
Ty <tyst3273@colorado.edu>
2023-10-16 20:59:55 -0600

    merge

---
+++ visa/test.py
@@ -0,0 +1,108 @@
+import pyvisa
+
+# ---------------------------------------------------------------------------------------------------
+
+class c_pyvisa:
+
+    # ----------------------------------------------------------------------------------------------
+
+    def __init__(self,debug=False):
+        
+        """
+        interface to pyvisa package
+        debug: whether or not to log pyvisa debugging info to screen
+        """
+
+        if debug:
+            pyvisa.log_to_screen()
+
+        self.resource_manager = pyvisa.ResourceManager()
+        
+        # holds resources
+        self.resources = {}
+
+    # ----------------------------------------------------------------------------------------------
+
+    def print_available_resources(self,try_to_connect=False):
+
+        """
+        loop over all available resources, try to connect, and print info if successful
+        """
+
+        rm = self.resource_manager
+
+        resources = rm.list_resources()
+        print('\navailable resources:\n'+'\n'.join(resources),'\n')
+
+        if try_to_connect:
+
+            for res in resources:   
+
+                print('\n------------------------------------------------------\n')
+                print('Resource:',res)
+
+                try:
+                    _instr = rm.open_resource(res)
+                except Exception as ex:
+                    print('\n*** ERROR ***\ncouldnt connect to resource\n')
+                    print(ex,'\n')
+                    continue
+
+                print('\n*** resource info ***')
+                print(_instr.query('*IDN?'))
+                _instr.close()
+
+    # ----------------------------------------------------------------------------------------------
+
+    def open_resource(self,resource_id,resource_handle='_'):
+
+        """
+        open a resource and add it to the resource dict
+        """
+
+        if resource_handle in self.resources:
+            msg = f'\n*** ERROR ***\na resource with the handle:\n \"{resource_handle}\"\n'
+            msg += 'already exists! pick a different handle\n'
+            print(msg)
+            return
+
+        try:
+            _instr = self.resource_manager.open_resource(resource_id)
+        except Exception as ex:
+            print('\n*** ERROR ***\ncouldnt connect to resource\n')
+            print(ex,'\n')
+            return
+        
+        self.resources[resource_handle] = _instr
+        print('\n*** opening resource ***')
+        print('resource:',resource_id)
+        print('handle:',resource_handle)
+        print('info:\n',_instr.query('*IDN?'))
+
+    # ----------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    pv = c_pyvisa()
+
+    #pv.print_available_resources(try_to_connect=True)
+    pv.open_resource('ASRL33::INSTR')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
