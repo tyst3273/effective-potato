3e689b4935742c4680927b7dbd0c33585623f917
tyst3273 <tyst3273@colorado.edu>
2024-05-17 09:36:12 -0600

    autocommit

--- compressor/compressor.py
+++ compressor/compressor.py
@@ -43,7 +43,7 @@ class c_compressor:
     
     # ----------------------------------------------------------------------------------------------
     
-    def __init__(self,file_name,num_steps=None,header_length=9):
+    def __init__(self,lammps_traj_file,hdf5_traj_file,num_steps=None,header_length=9):
         
         """
         class to read lammps trajectory file and write it to hdf5 file. this file is designed 
@@ -51,14 +51,54 @@ class c_compressor:
         """
         
         self.header_length = header_length
-        self.file_name = file_name
+        self.lammps_traj_file = lammps_traj_file
+        self.hdf5_traj_file = hdf5_traj_file
 
         #self.positions = np.zeros(self.num_atoms,)
         #self.types = np.zeros(self.num_atoms)
 
-        if rank == 0:
-            self._get_meta_data()
-            self._split_trajectory_over_ranks()
+        #if rank == 0:
+        #    self._get_meta_data(num_steps)
+        #    self._split_trajectory_over_ranks()
+
+        self._get_meta_data(num_steps)
+        self._broadcast_steps()
+
+        # all ranks have to create datasets etc.
+        self._init_hdf5_file()
+
+    # ----------------------------------------------------------------------------------------------
+
+    def _broadcast_steps(self):
+
+        """
+        """
+
+        pass        
+
+    # ----------------------------------------------------------------------------------------------
+
+    def _init_hdf5_file(self):
+
+        """
+        create output hdf5 file and pos, type, box_vector arrays in it
+        """
+
+        _num_steps = self.num_steps
+        _num_atoms = self.num_atoms
+
+        print('a')
+        if have_mpi:
+            db = h5py.File(self.hdf5_traj_file,'w',driver='mpio',comm=comm)
+        else:
+            db = h5py.File(self.hdf5_traj_file,'w')
+
+        db.create_dataset('cartesian_pos',shape=(_num_steps,_num_atoms,3),dtype=float)        
+        db.create_dataset('types',shape=_num_atoms,dtype=float)
+        db.create_dataset('box_vectors',shape=(_num_steps,3,3),dtype=float)
+        
+        print('b')
+        db.close()
 
     # ----------------------------------------------------------------------------------------------
 
@@ -69,10 +109,11 @@ class c_compressor:
         """
 
         self.time_steps = np.arange(self.num_steps)
-        self.time_steps_on_ranks = np.array_split(self.time_steps,size)
+        self.steps_on_ranks = np.array_split(self.time_steps,size)
+        self.num_steps_on_ranks = [len(_) for _ in self.steps_on_ranks]
 
-        print('\ntime steps on ranks:')
-        [print(f'rank[{ii}]:',_steps) for ii, _steps in enumerate(self.time_steps_on_ranks)]
+        print('\nnum steps on ranks:')
+        [print(f'rank[{rank}]:',num) for rank, num in enumerate(self.num_steps_on_ranks)]
 
     # ----------------------------------------------------------------------------------------------
     
@@ -84,20 +125,20 @@ class c_compressor:
         number of lines
         """
         
-        with open(self.file_name,'r') as _f:
+        with open(self.lammps_traj_file,'r') as _f:
             for ii in range(3):
                 _f.readline()
             self.num_atoms = int(_f.readline().strip())
             
         if num_steps is None:
-            with open(self.file_name, "rb") as _f:
+            with open(self.lammps_traj_file, "rb") as _f:
                 self.num_lines = sum(1 for _ in _f)
             self.num_steps = int(self.num_lines/(self.header_length+self.num_atoms))
         else:
             self.num_steps = int(num_steps)
             self.num_lines = (self.num_atoms+self.header_length)*self.num_steps
         
-        print('\nfile name:',self.file_name)
+        print('\nfile name:',self.lammps_traj_file)
         print('num atoms:',self.num_atoms)
         print('num lines:',self.num_lines)
         print('num steps:',self.num_steps)
@@ -109,8 +150,9 @@ class c_compressor:
 
 # --------------------------------------------------------------------------------------------------
 
-file_name = 'pos_trimmed.dat'
-compressor = c_compressor(file_name) 
+lammps_traj_file = 'pos.dat'
+hdf5_traj_file = 'pos.hdf5'
+compressor = c_compressor(lammps_traj_file,hdf5_traj_file,num_steps=2501) 
 
 
 
