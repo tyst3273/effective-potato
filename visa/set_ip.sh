c609ac8f11b6f8cffab2da050fa18cf317d8e3b3
tyst3273 <tyst3273@colorado.edu>
2025-09-05 07:51:10 -0600

    auto commit

--- visa/set_ip.sh
+++ visa/set_ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo ip addr add 192.168.0.10/24 dev enp5s0                     
-sudo ip link set enp5s0 up                                      
-ping 192.168.0.2     
+sudo ip addr add 192.168.0.10/24 dev enp5s0 # add my pc's ip to the ethernetwork
+sudo ip link set enp5s0 up # i dunno. do more add stuff.
+ping 192.168.0.2 # check if we can talk to powersupply
