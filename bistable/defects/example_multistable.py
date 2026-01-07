a8a98485e527d815ce552cc06d06df68b2bb6d8e
tyst3273 <tyst3273@colorado.edu>
2026-01-07 07:33:01 -0600

    auto commit

---
+++ bistable/defects/example_multistable.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+gamma = 5.0
+
+y = 0.1
+x = np.linspace(0,4,1000)
+
+fig, ax = plt.subplots(1,2,figsize=(8,3))
+
+f = gamma * np.exp(-1/(x+y))
+
+ax[0].plot(x,f-x,c='m',label='$L(x)$')
+ax[0].axhline(0,lw=1,ls='--',c='k')
+ax[0].legend(loc='upper right')
+
+ax[1].plot(x,f,c='b',label='$\gamma e^{1/(x+y)}$')
+ax[1].plot(x,x,c='r',label='x')
+ax[1].legend()
+
+ax[0].set_xlabel('x')
+ax[1].set_xlabel('x')
+
+fig.suptitle('$\gamma=5.0,~y=0.1$')
+
+plt.savefig('example_dot_n.png',dpi=200,format='png',bbox_inches='tight')
+plt.show()
