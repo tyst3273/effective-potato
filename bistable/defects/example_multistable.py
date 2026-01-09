6cceef3f3a70f1fcf9b06d2d2479b235a93a9bf8
tyst3273 <tyst3273@colorado.edu>
2026-01-08 18:49:19 -0600

    auto commit

--- bistable/defects/example_multistable.py
+++ bistable/defects/example_multistable.py
@@ -12,14 +12,19 @@ f = gamma * np.exp(-1/(x+y))
 
 ax[0].plot(x,f-x,c='m',label='$L(x)$')
 ax[0].axhline(0,lw=1,ls='--',c='k')
-ax[0].legend(loc='upper right')
+ax[0].legend(loc='upper right',frameon=False)
 
 ax[1].plot(x,f,c='b',label='$\gamma e^{1/(x+y)}$')
 ax[1].plot(x,x,c='r',label='x')
-ax[1].legend()
+ax[1].legend(frameon=False)
 
 ax[0].set_xlabel('x')
 ax[1].set_xlabel('x')
+
+# ax[1].set_yscale('log')
+
+ax[0].axis([0,gamma,-1,1.25])
+ax[1].axis([0,gamma,0,4.5])
 
 fig.suptitle('$\gamma=5.0,~y=0.1$')
 
