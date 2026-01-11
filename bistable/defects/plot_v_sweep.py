44aed0f2686199866d8440a355f1227e652620c5
tyst3273 <tyst3273@colorado.edu>
2026-01-10 20:37:56 -0600

    auto commit

--- bistable/defects/plot_v_sweep.py
+++ bistable/defects/plot_v_sweep.py
@@ -7,42 +7,52 @@ def get_data(filename):
         
     with h5py.File(filename,'r') as db:
 
-        n_lo = db['n_lo'][...]
-        n_hi = db['n_hi'][...]
-        x_lo = db['x_lo'][...]
-        x_hi = db['x_hi'][...]
+        n = db['n'][...]
+        x = db['x'][...]
         v = db['v'][...]
         y = db['y'][...]
+
+    n_lo = n[:,0]
+    n_hi = n[:,-1]
+
+    x_lo = x[:,0]
+    x_hi = x[:,-1]
+    
+    n_hi[x_hi == 0.0] = np.nan
+    x_hi[x_hi == 0.0] = np.nan
 
     return n_lo, n_hi, x_lo, x_hi, v, y
 
 fig, ax = plt.subplots(3,2,figsize=(4.5,6),gridspec_kw={'hspace':0.15,'wspace':0.1})
 
 n_lo, n_hi, x_lo, x_hi, v, y = get_data('results_v_sweep_y_0.010.h5')
-ax[0,0].plot(v,n_lo,c='b',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[0,0].plot(v,n_hi,c='b',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[0,1].plot(v,x_lo,c='r',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[0,1].plot(v,x_hi,c='r',lw=1,ls=(0,(2,1)),marker='o',ms=1)
+
+ax[0,0].plot(v,n_lo,c='b',lw=0,marker='o',ms=1)
+ax[0,1].plot(v,x_lo,c='r',lw=0,marker='o',ms=1)
+
+ax[0,1].plot(v,x_hi,c='r',lw=0,marker='o',ms=1)
+ax[0,0].plot(v,n_hi,c='b',lw=0,marker='o',ms=1)
+
 ax[0,0].annotate('(a)',xy=(0.05,0.85),xycoords='axes fraction',c='k')
 ax[0,0].annotate('y=0.01',xy=(0.4,0.85),xycoords='axes fraction',c='k')
 ax[0,1].annotate('(b)',xy=(0.05,0.85),xycoords='axes fraction',c='k')
 ax[0,1].annotate('y=0.01',xy=(0.4,0.85),xycoords='axes fraction',c='k')
 
 n_lo, n_hi, x_lo, x_hi, v, y = get_data('results_v_sweep_y_0.100.h5')
-ax[1,0].plot(v,n_lo,c='b',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[1,0].plot(v,n_hi,c='b',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[1,1].plot(v,x_lo,c='r',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[1,1].plot(v,x_hi,c='r',lw=1,ls=(0,(2,1)),marker='o',ms=1)
+ax[1,0].plot(v,n_lo,c='b',lw=0,marker='o',ms=1)
+ax[1,0].plot(v,n_hi,c='b',lw=0,marker='o',ms=1)
+ax[1,1].plot(v,x_lo,c='r',lw=0,marker='o',ms=1)
+ax[1,1].plot(v,x_hi,c='r',lw=0,marker='o',ms=1)
 ax[1,0].annotate('(c)',xy=(0.05,0.85),xycoords='axes fraction',c='k')
 ax[1,0].annotate('y=0.10',xy=(0.4,0.85),xycoords='axes fraction',c='k')
 ax[1,1].annotate('(d)',xy=(0.05,0.85),xycoords='axes fraction',c='k')
 ax[1,1].annotate('y=0.10',xy=(0.4,0.85),xycoords='axes fraction',c='k')
 
 n_lo, n_hi, x_lo, x_hi, v, y = get_data('results_v_sweep_y_0.250.h5')
-ax[2,0].plot(v,n_lo,c='b',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[2,0].plot(v,n_hi,c='b',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[2,1].plot(v,x_lo,c='r',lw=1,ls=(0,(2,1)),marker='o',ms=1)
-ax[2,1].plot(v,x_hi,c='r',lw=1,ls=(0,(2,1)),marker='o',ms=1)
+ax[2,0].plot(v,n_lo,c='b',lw=0,marker='o',ms=1)
+ax[2,0].plot(v,n_hi,c='b',lw=0,marker='o',ms=1)
+ax[2,1].plot(v,x_lo,c='r',lw=0,marker='o',ms=1)
+ax[2,1].plot(v,x_hi,c='r',lw=0,marker='o',ms=1)
 ax[2,0].annotate('(e)',xy=(0.05,0.85),xycoords='axes fraction',c='k')
 ax[2,0].annotate('y=0.50',xy=(0.4,0.85),xycoords='axes fraction',c='k')
 ax[2,1].annotate('(f)',xy=(0.05,0.85),xycoords='axes fraction',c='k')
